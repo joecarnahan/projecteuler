267d34c574aba85c7af4305e7cb398e60fda8914
Joe Carnahan <joseph.carnahan@gmail.com>
2011-10-02 19:38:53 -0400

    Added (unimpressive) timing stats

--- 002/Efficient.scala
+++ 002/Efficient.scala
@@ -3,23 +3,22 @@
  * isEven() check is skipped in favor of just summing every third
  * Fibonacci number.
  *
+ * Note that the "improved" version does not seem to take any less time
+ * than the naive version, as both complete almost instantly.
+ *
+ * Calling this with no command-line arguments causes it to run once and
+ * and show the sum of all even Fibonacci numbers less than 4000000.
+ * Calling this with a single number as the command-line argument causes it
+ * to run repeatedly finding the sum of all even Fibonacci numbers less than
+ * than that number and to print out the time required to compute that result.
+ *
  * @author Joe Carnahan (joseph.carnahan@gmail.com)
  */
 object Efficient {
 
-//sum=0
-//a=1
-//b=1
-//c=a+b
-//while c<limit
-   //sum=sum+c
-   //a=b+c
-   //b=c+a
-   //c=a+b
+  def computeValuesUpTo(limit: Long): Long = {
 
-  def computeValuesUpTo(limit: Int): Int = {
-
-    def computeValues(sum: Int, a: Int, b: Int, c: Int): Int =
+    def computeValues(sum: Long, a: Long, b: Long, c: Long): Long =
       if (c >= limit)
         sum
       else {
@@ -33,8 +32,13 @@ object Efficient {
 
   }
 
-
   def main(args: Array[String]) = 
-    println(computeValuesUpTo(4000000))
+    if (args.length == 1)
+      println("Efficient algorithm required an average of " + 
+              Timer.timeInMilliseconds(() =>
+                computeValuesUpTo(java.lang.Long.parseLong(args(0)))) +
+              " milliseconds.")
+    else
+      println(computeValuesUpTo(4000000L))
 
 }
--- 002/Naive.scala
+++ 002/Naive.scala
@@ -1,28 +1,38 @@
 /**
  * My solution to problem 2 of Project Euler.
  *
+ * Calling this with no command-line arguments causes it to run once and
+ * and show the sum of all even Fibonacci numbers less than 4000000.
+ * Calling this with a single number as the command-line argument causes it
+ * to run repeatedly finding the sum of all even Fibonacci numbers less than
+ * than that number and to print out the time required to compute that result.
+ *
  * @author Joe Carnahan (joseph.carnahan@gmail.com)
  */
 object Naive {
 
-  def isEven(toTest: Int) = ((toTest % 2) == 0)
+  def isEven(toTest: Long) = ((toTest % 2) == 0)
 
-  def computeValuesUpTo(limit: Int): Int = {
+  def computeValuesUpTo(limit: Long): Long = {
 
-    def computeValues(curr: Int, prev: Int, sum: Int): Int =
+    def computeValues(curr: Long, prev: Long, sum: Long): Long =
       if (curr >= limit)
         sum
       else
         computeValues(curr + prev, curr,
                       if (isEven(curr)) sum + curr else sum)
-                        
 
     computeValues(1, 0, 0)
 
   }
 
-
   def main(args: Array[String]) = 
-    println(computeValuesUpTo(4000000))
+    if (args.length == 1)
+      println("Naive algorithm required an average of " + 
+              Timer.timeInMilliseconds(() =>
+                computeValuesUpTo(java.lang.Long.parseLong(args(0)))) +
+              " milliseconds.")
+    else
+      println(computeValuesUpTo(4000000L))
 
 }
