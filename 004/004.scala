a741f520470d1b06b8f21aa0f82aa7485376cfcf
Joe Carnahan <joseph.carnahan@gmail.com>
2011-10-05 19:56:29 -0400

    Current approach doesn't work because just counting down from 999 doesn't give biggest palindrome

--- 004/004.scala
+++ 004/004.scala
@@ -7,20 +7,48 @@ object _004 {
 
   val defaultLimit = 1000
 
-  // Thoughts:
-  //  - Palindromes must have even numbers of digits
-  //     - How can we predict how many digits there will be?
-  //  - Want to start trying numbers close to 1000 and work down
-  //  - How can we check if a product is a palindrome?
-  //     - String conversion
-  //     - Other ideas?
+  def isPalindrome(toCheck: String): Boolean = 
+    if (toCheck.size < 2)
+      true
+    else if (toCheck(0) == toCheck((toCheck.size - 1)))
+      isPalindrome(toCheck.substring(1, toCheck.size - 1))
+    else
+      false
 
-  //def isPalindrome = 
+  // def isPalindrome(toCheck: Long): Boolean = isPalindrome(toCheck.toString)
+  // debug
+  def isPalindrome(toCheck: Long): Boolean = {
+    val s = toCheck.toString
+    if (isPalindrome(s)) {
+      println(s + " is a palindrome")
+      true
+    }
+    else {
+      println(s + " is not a palindrome")
+      false
+    }
+  }
+  // enddebug
 
-  //def findLargestPalindromeProductOfNumbersLessThan(limit: Long) = 
+  def findLargestPalindromeProductOfNumbersLessThan(limit: Long) = {
     
-    //def search(a: Long, b: Long): Long =
-      //if (
+    def search(a: Long, b: Long): Long =
+      // debug
+      {
+        println("Trying " + a.toString + " and " + b.toString)
+      // enddebug
+      if (isPalindrome(a * b))
+        a * b
+      else if (a == b)
+        search(a - 1, limit - 1)
+      else
+        search(a, b - 1)
+      // debug
+      }
+      // enddebug
+
+    search(limit - 1, limit - 1)
+  }
 
   def runOn(limit: Long) = {
     println(findLargestPalindromeProductOfNumbersLessThan(limit))
