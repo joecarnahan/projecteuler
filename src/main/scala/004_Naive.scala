8aafecdc69369d1a9666c7f8fe496e6747ef84cb
Joe Carnahan <joseph.carnahan@gmail.com>
2011-10-05 22:57:53 -0400

    Made the naive solution for problem 4 a lot less naive (but still no step-by-11 trick)

--- src/main/scala/004_Naive.scala
+++ src/main/scala/004_Naive.scala
@@ -20,20 +20,22 @@ object _004_Naive {
 
   def findLargestPalindromeProductOfNumbersLessThan(limit: Long) = {
 
-    def search(a: Long, b: Long, largest: Long): Long =
-      if (a == limit)
+    def search(a: Long, b: Long, largest: Long): Long = 
+      if (a <= 1)
         largest
-      else if (b == limit)
-        search(a + 1, a + 1, largest)
+      else if (b < a)
+        search(a - 1, limit - 1, largest)
       else {
         val product = a * b
-        if (isPalindrome(product) && (product > largest))
-          search(a, b + 1, product)
+        if (product <= largest)
+          search(a - 1, limit - 1, largest)
+        else if (isPalindrome(product))
+          search(a - 1, limit - 1, product)
         else
-          search(a, b + 1, largest)
+          search(a, b - 1, largest)
       }
 
-    search(1, 1, 1)
+    search(limit - 1, limit - 1, 1)
   }
 
   val defaultLimit = 1000
