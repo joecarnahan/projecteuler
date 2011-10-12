7dcf6f05840b2a150d355dc08b1385cb2abd3f38
Joe Carnahan <joseph.carnahan@gmail.com>
2011-10-12 16:38:47 -0400

    Implemented solution using closed-form function for sum of squares

--- src/main/scala/006.scala
+++ src/main/scala/006.scala
@@ -1,5 +1,15 @@
 /**
  * My solution to problem 6 of Project Euler.
+ * <p>
+ * Before I could figure out the closed-form solution for the 
+ * sum-of-squares function <em>f()</em>, I needed a hint that I
+ * should start by looking at <em>f(0)</em>, <em>f(1)</em>,
+ * <em>f(2)</em>, and <em>f(3)</em>, and solve for the constants
+ * in a cubic equation that would produce those values.  
+ * <p>
+ * Even then, it took me a few tries to get my algebra right -
+ * It's been a long time since I had to solve multiple equations
+ * for multiple unknowns. ;-)
  *
  * @author Joe Carnahan (joseph.carnahan@gmail.com)
  */
@@ -28,6 +38,29 @@ object _006 {
   }
 
   /**
+   * Computes the sum of all natural numbers up to and including the given
+   * number.
+   *
+   * @param limit
+   *          the limit of the arithmetic progression
+   * @return the sum of the integers from 1 through <code>limit</code>
+   */
+  def arithmeticProgression(limit: Long): Long =
+    limit * (limit + 1) / 2
+
+  /**
+   * Computes the sum of all squares of all natural numbers up to and
+   * including the given number.
+   *
+   * @param limit
+   *          the largest number to square and add to this sum
+   * @return the sum of the squares of the integers from 1 through
+   *         <code>limit</code>
+   */
+  def sumOfSquares(limit: Long): Long =
+    limit * ((2 * limit) + 1) * (limit + 1) / 6
+
+  /**
    * Computes the difference between the square of the sum of numbers up to
    * given value and the sum of the squares of the numbers up to the given
    * value.
@@ -38,8 +71,10 @@ object _006 {
    * @return the difference between the square of the sum and the sum of the
    *         squares
    */
-  def differenceBetweenSumOfSquaresAndSquareOfSums(limit: Long): Long = 
-    naiveSolution(limit)
+  def differenceBetweenSumOfSquaresAndSquareOfSums(limit: Long): Long = {
+    val sum = arithmeticProgression(limit)
+    (sum * sum) - sumOfSquares(limit)
+  }
 
   val defaultLimit = 100L
 
