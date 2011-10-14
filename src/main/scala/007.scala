1a902edc31712bd4b1ebed12e47d2d9e55358cca
Joe Carnahan <joseph.carnahan@gmail.com>
2011-10-14 04:19:58 -0400

    Implemented a working solution to problem 7

---
+++ src/main/scala/007.scala
@@ -0,0 +1,23 @@
+/**
+ * My solution to problem 7 of Project Euler.
+ * <p>
+ * The new code that I wrote for this problem is mostly in the new
+ * implementation of the Sieve of Erasthones.  For small values of
+ * <em>N</em>, this file worked already even with my old implementation
+ * of the Sieve.
+ *
+ * @author Joe Carnahan (joseph.carnahan@gmail.com)
+ */
+object _007 {
+
+  def getNthPrime(index: Long): Long =
+    Common.sieveOfErasthones.drop(index.asInstanceOf[Int] - 1).head
+
+  val defaultLimit = 10001L
+
+  def main(args: Array[String]) =
+    Runner.run(args, defaultLimit,
+               getNthPrime(_).toString,
+               "Solution to problem 7")
+
+}
--- src/main/scala/Common.scala
+++ src/main/scala/Common.scala
@@ -9,14 +9,22 @@
 object Common {
 
   /**
-   * Builds a stream of prime numbers.
+   * Gets all of the natural numbers starting with the given number.
+   *
+   * @param value
+   *          the number to start from
+   * @return a stream of numbers starting from the given one
+   */
+  def from(value: Long): Stream[Long] =
+    Stream.cons(value, from(value + 1))
+
+  /**
+   * Simple implementation of the Sieve of Erasthones, as demonstrated in
+   * the documentation for the Scala Stream class.
    *
    * @return All of the prime numbers, evaluated lazily
    */
-  def sieveOfErasthones(): Stream[Long] = {
-
-    def from(value: Long): Stream[Long] =
-      Stream.cons(value, from(value + 1))
+  def simpleSieveOfErasthones(): Stream[Long] = {
 
     def sieve(stream: Stream[Long]): Stream[Long] =
       Stream.cons(stream.head, 
@@ -24,6 +32,57 @@ object Common {
                                            (x % stream.head) != 0 )))
 
     sieve(from(2))
+
+  }
+
+  /**
+   * Builds a stream of prime numbers.
+   *
+   * @return All of the prime numbers, evaluated lazily
+   */
+  def sieveOfErasthones(): Stream[Long] = {
+
+    /**
+     * Yields a function to check if a given number is divisible by
+     * any of the given prime numbers.  We assume that the given
+     * numbers are sorted in ascending order, so that we can cut off
+     * the search if we get to a prime number that is larger than
+     * the square root of the given number.
+     *
+     * @param primes
+     *          the list of prime numbers to check
+     * @return a function that checks if a number is prime relative
+     *         to the given list of prime numbers
+     */
+    def divisibleByAny(primes: List[Long]): Long => Boolean =
+
+      (candidate: Long) => {
+
+        val squareRoot = scala.math.sqrt(candidate).asInstanceOf[Long]
+
+        def search(list: List[Long]): Boolean =
+          list match {
+            case Nil => false
+            case head :: tail => 
+              if (head > squareRoot)
+                false
+              else if ((candidate % head) == 0)
+                true 
+              else 
+                search(tail)
+          }
+
+        search(primes)
+
+      }
+
+    def sieve(stream: Stream[Long], primes: List[Long]): Stream[Long] =
+      Stream.cons(stream.head, 
+                  sieve(stream.tail.dropWhile(divisibleByAny(primes.reverse)),
+                        stream.head :: primes))
+
+    sieve(from(2), List[Long]())
+
   }
 
   /**
