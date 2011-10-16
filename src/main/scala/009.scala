58f6cafe72051bc8e9139dbce18ec2d9897b0ea9
Joe Carnahan <joseph.carnahan@gmail.com>
2011-10-16 10:17:27 -0400

    Got a working solution to Problem 9

--- src/main/scala/009.scala
+++ src/main/scala/009.scala
@@ -5,16 +5,46 @@
  */
 object _009 {
 
+  /**
+   * Tests if the given triple of numbers is a 
+   * Pythagorean triple.
+   *
+   * @param a 
+   *          first number to test
+   * @param b 
+   *          second number to test
+   * @param c 
+   *          third number to test
+   * @return <code>true</code> if and only if (a*a) + (b*b) = (c*c)
+   */
+  def isTriple(a: Int, b: Int, c: Int): Boolean = 
+    ((a * a) + (b * b)) == (c * c) 
+
+  /**
+   * Searches for a Pythagorean triplet that sums to the given value.
+   * <p>
+   * Starts with <code>a = 1</code>, <code>b = 2</code>, and
+   * <code>c = value - (a + b)</code>.  Then, and it increments
+   * <code>a</code> until it reaches <code>b</code>.  At that point, it
+   * increments <code>b</code> and starts over again with <code>a = 1</code>.
+   *
+   * @param value
+   *          the number to which the three numbers in the Pythagorean
+   *          triple should sum
+   * @return a list of three integers consisting of a Pythagorean
+   *         triple that sums to <code>value</code>, or an empty list if
+   *         no Pythagorean triple sums to <code>value</code>
+   */
   def getPythagoreanTriplet(value: Long): List[Int] = {
 
     def sum = value.toInt
 
     def search(a: Int, b: Int, c: Int): List[Int] =
-      if ((a * a) + (b * b) == (c * c))
+      if (isTriple(a, b, c))
         List(a, b, c)
-      else if (a == (b - 1)) {
+      else if ((a >= (b - 1)) || (a >= c)) {
         val newB = b + 1
-        if (newB > (sum / 3)) 
+        if (newB > (sum / 2)) 
           List[Int]()          // No Pythagorean triple exists
         else {
           val newA = 1
@@ -30,12 +60,57 @@ object _009 {
 
   }
 
+  /**
+   * Searches for a Pythagorean triplet that sums to the given value.
+   *
+   * @param value
+   *          the number to which the three numbers in the Pythagorean
+   *          triple should sum
+   * @return a list of three integers consisting of a Pythagorean
+   *         triple that sums to <code>value</code> 
+   * @throws RuntimeException
+   *           if no Pythagorean triple exists that sums to 
+   *           <code>value</code>
+   */
   def getPythagoreanTripletOrError(value: Long): List[Int] = {
     val result = getPythagoreanTriplet(value)
     if (result.isEmpty)
       sys.error("No Pythagorean triplet sums to " + value)
     else
       result
+  }
+
+  /**
+   * Brute-force method for finding Pythagorean triplets.  Used for
+   * debugging this algorithm.
+   *
+   * @param n
+   *          the number of triplets to get
+   */
+  def getNTriplets(n: Int): List[List[Int]] = {
+
+    def increment(a: Int, b: Int, c: Int): (Int, Int, Int) = 
+      if (a == (b - 1))
+        if (b == (c - 1))
+          (1, 2, c + 1)
+        else
+          (1, b + 1, c)
+      else
+        (a + 1, b, c)
+    
+    def search(a: Int, b: Int, c: Int, result: List[List[Int]]): List[List[Int]] =
+      if (result.size == n)
+        result
+      else {
+        val (newA, newB, newC) = increment(a, b, c)
+        if (isTriple(a, b, c))
+          search(newA, newB, newC, List(a, b, c) :: result)
+        else
+          search(newA, newB, newC, result)
+      }
+      
+    search(1, 2, 3, List[List[Int]]())
+
   }
 
   val defaultValue = 1000L
