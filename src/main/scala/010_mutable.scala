4313a3867c37ec870d68fcd6f00c4183698e1b3f
Joe Carnahan <joseph.carnahan@gmail.com>
2011-10-24 21:56:01 -0400

    Added a bitset-based solution to Problem 10 with much better performance

---
+++ src/main/scala/010_mutable.scala
@@ -0,0 +1,41 @@
+/**
+ * My solution to problem 10 of Project Euler using a mutable bit set
+ * to implement the Sieve of Erasthones.
+ *
+ * @author Joe Carnahan (joseph.carnahan@gmail.com)
+ */
+object _010_mutable {
+
+  /**
+   * Gets all of the prime numbers that are less than the given value.
+   */
+  def sieveOfErasthones(limit: Int): Seq[Int] = {
+    // Construct the list of numbers to only include odd values
+    val numbers = scala.collection.mutable.BitSet((3 to limit by 2): _*)
+    // Then, put 2 back into the list
+    numbers += 2
+    val squareRoot = scala.math.sqrt(limit)
+    var prime = 3
+    while (prime <= squareRoot) {
+      var j = prime * prime
+      while (j <= limit) {
+        numbers -= j
+        j += prime
+      }
+      prime += 2
+      while (!numbers(prime)) prime += 1;
+    }
+    numbers.toStream
+  }
+
+  def getSumOfPrimesLessThan(limit: Long): Long =
+    sieveOfErasthones(limit.asInstanceOf[Int]).map(_.asInstanceOf[Long]).sum
+
+  val defaultLimit = 2000000L
+
+  def main(args: Array[String]) =
+    Runner.runLong(args, defaultLimit,
+                   getSumOfPrimesLessThan(_).toString,
+                   "Solution to problem 10")
+
+}
