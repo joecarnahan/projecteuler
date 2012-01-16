3a71b820915b5e0639655c87c26f33b9d24102a2
Joe Carnahan <joseph.carnahan@gmail.com>
2012-01-16 17:30:12 -0500

    Updated run.sh to run all tests

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAIN_CLASSES="_001_Naive _001_Efficient _002_Naive _002_Efficient _003 _004_Naive _004_Efficient _005 _006 _007 _008 _009 _010_Functional _010_Mutable _011 _012_Naive _012_Efficient TreeTest"
+MAIN_CLASSES="_001_Naive _001_Efficient _002_Naive _002_Efficient _003 _004_Naive _004_Efficient _005 _006 _007 _008 _009 _010_functional _010_mutable _011 _012_Naive _012_Efficient TreeTest"
 BUILD_DIR="target/scala-2.9.1/classes"
 
 if [ $# -eq 0 ]
