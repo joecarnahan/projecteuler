a61a307603d0a0b5eba1f12289c2435a0e82afd1
Joe Carnahan <joseph.carnahan@gmail.com>
2012-05-16 16:07:41 +0100

    Working on a version that short-circuits using powers of 2

--- src/main/scala/014.scala
+++ src/main/scala/014.scala
@@ -13,19 +13,35 @@ object _014 {
 
   def isEven(toTest: Long) = ((toTest % 2) == 0)
 
+  def buildPowerMap: Map[Long, Long] = {
+    def buildPowerMap(exponent: Long, map: Map[Long,Long]): Map[Long, Long] =
+      if ((1L << exponent) > (Long.MaxValue / 2))
+        map + (exponent -> (1L << exponent))
+      else
+        buildPowerMap(exponent + 1, map + (exponent -> (1L << exponent)))
+    buildPowerMap(0, Map())
+  }
+
+  val powerMap: Map[Long, Long] = buildPowerMap
+
+  def powerOfTwo(toTest: Long): Option[Long] = powerMap.get(toTest)
+
   def findLongestSequenceStartingUnder(limit: Long): Long = {
     case class Solution(startingValue: Long, currentValue: Long, length: Long)
     def exploreSolution(startingValue: Long): Solution = {
       def exploreSolution(current: Solution): Solution = {
-        if (current.currentValue == 1)
-          current
-        else
-          exploreSolution(Solution(current.startingValue,
-                                   if (isEven(current.currentValue))
-                                     current.currentValue / 2
-                                   else
-                                     current.currentValue * 3 + 1, 
-                                   current.length + 1))
+        // TODO Why doesn't this work? RESUME HERE
+        powerOfTwo(current.currentValue) match {
+          case None => 
+            exploreSolution(Solution(current.startingValue,
+                                     if (isEven(current.currentValue))
+                                       current.currentValue / 2
+                                     else
+                                       current.currentValue * 3 + 1, 
+                                     current.length + 1))
+          case Some(exponent) =>
+            Solution(current.startingValue, 1, current.length + exponent - 1)
+        }
       }
       exploreSolution(Solution(startingValue, startingValue, 1))
     }
