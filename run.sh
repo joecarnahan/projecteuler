f327fadc97ad1b755b6106ac12db9c5d9efc9142
Joe Carnahan <joseph.carnahan@gmail.com>
2012-07-07 12:23:31 -0400

    Updated run.sh to include new solutions and to skip one super-slow solution

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-MAIN_CLASSES="_001_Naive _001_Efficient _002_Naive _002_Efficient _003 _004_Naive _004_Efficient _005 _006 _007 _008 _009 _010_functional _010_mutable _011 _012_Naive _012_Efficient TreeTest"
-BUILD_DIR="target/scala-2.9.1/classes"
+#MAIN_CLASSES="_001_Naive _001_Efficient _002_Naive _002_Efficient _003 _004_Naive _004_Efficient _005 _006 _007 _008 _009 _010_functional _010_mutable _011 _012_Naive _012_Efficient _013 _014_Naive _014 TreeTest"
+MAIN_CLASSES="_001_Naive _001_Efficient _002_Naive _002_Efficient _003 _004_Naive _004_Efficient _005 _006 _007 _008 _009 _010_mutable _011 _012_Naive _012_Efficient _013 _014_Naive _014 TreeTest"
+BUILD_DIR="target/scala-2.9.2/classes"
 
 if [ $# -eq 0 ]
 then
